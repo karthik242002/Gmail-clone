b1b0d377cc6c81595bdb5769b5348ecc8007a487
karthik242002 <karthiksiva153@gmail.com>
2023-02-05 16:18:31 +0530

    created next/auth and added api key

---
+++ src/pages/_app.js
@@ -0,0 +1,10 @@
+import '../app/globals.css'
+import { SessionProvider } from "next-auth/react"
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+
+    )
+}
